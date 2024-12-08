c28927e138ea039fa1fe97f1551e8d98d6fecddd
Wojciech Gajda <wggajda@gmail.com>
2024-12-08 10:04:21 +0100

    add: hint in corner

--- src/assistant/telescope_assistant.py
+++ src/assistant/telescope_assistant.py
@@ -45,6 +45,21 @@ class TelescopeAssistant():
             x = int(resolution[0] * (0.5 + relative_azimuth / fov[0]))
             y = int(resolution[1] * (0.5 - relative_elevation / fov[1]))
             self.spot_celestial(canvas, x, y)
+
+        def rad2deg(rad):
+            deg = rad * 180 / math.pi
+            if deg < 0:
+                deg += 360
+            return deg
+
+        azimuth_text = f"AZIMUT: {rad2deg(orientation[0]):.2f}"
+        elevation_text = f"ELEVATION: {rad2deg(orientation[1]):.2f}"
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        font_scale = 1
+        color = (255, 255, 255)
+        thickness = 2
+        cv2.putText(canvas, azimuth_text, (10, resolution[1] - 60), font, font_scale, color, thickness, cv2.LINE_AA)
+        cv2.putText(canvas, elevation_text, (10, resolution[1] - 30), font, font_scale, color, thickness, cv2.LINE_AA)
           
         argb_frame = cv2.cvtColor(canvas, cv2.COLOR_BGR2BGRA)
         argb_frame[:, :, 3] = 255
